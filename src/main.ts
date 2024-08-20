6fa38aca98048dbc9a6fdea5215301dbde4b9bc2
Darth Jar Jar <fabiocarrico1999@gmail.com>
2024-08-20 21:59:09 +0100

    production test

--- src/main.ts
+++ src/main.ts
@@ -6,22 +6,26 @@ import { enableProdMode } from "@angular/core";
 import * as Sentry from "@sentry/angular";
 
 
-Sentry.init({
-  dsn: "https://9848c8bda6b9e8925a9cf5b2597078f5@o4507810809839616.ingest.de.sentry.io/4507811051798608",
-  integrations: [
-    Sentry.browserTracingIntegration(),
-    Sentry.replayIntegration(),
-  ],
-  // Tracing
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
-  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
-  // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-});
+if (process.env['NODE_ENV'] === 'production') {
+  Sentry.init({
+    dsn: "https://9848c8bda6b9e8925a9cf5b2597078f5@o4507810809839616.ingest.de.sentry.io/4507811051798608",
+    integrations: [
+      Sentry.browserTracingIntegration(),
+      Sentry.replayIntegration(),
+    ],
+    // Tracing
+    tracesSampleRate: 1.0, //  Capture 100% of the transactions
+    // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+    tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+    // Session Replay
+    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  });
+  
+  enableProdMode();
+}
 
-enableProdMode();
+
 
 
 platformBrowserDynamic().bootstrapModule(AppModule, {
